0fb62703b3cbd8e1906b0e676137451e477b40b2
Zitha Makasi <zithamakasi179@gmail.com>
2026-09-15 20:13:31 +0200

    Updated some JavaScript

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function fetch(event) {
   let apiKey = "t0b1d0fae05ad3dd06fe36a44o3ff11e";
   let prompt = `User input: Generate a Xhosa traditional fact about ${input.value}`;
   let context =
-    "You are a Xhosa descendant and you have a deep knownledge of the traditions and roots of the Xhosa bloodline which you are going to share with those that are not in the Xhosa tribe. Your mission is to generate a 7 lines answer.Make sure to follow the user input. Please dont mix up the Zulu culture and Xhosa culture only give answers according to the xhosa culture. Add 'SheCodes AI' in <strong>and write it in a separate line from the last sentance using a <br/> and write it at the end of the answers.";
+    "You are a Xhosa descendant and you have a deep knownledge of the traditions and roots of the Xhosa bloodline which you are going to share with those that are not in the Xhosa tribe. Your mission is to generate a 7 lines answer.Make sure to follow the user input and with the answers provide an image according to the user input. Please dont mix up the Zulu culture and Xhosa culture only give answers according to the xhosa culture. Add 'SheCodes AI' in <strong> and write it in a separate line from the last sentance using a <br/> and write it at the end of the answers.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating answer");
@@ -28,5 +28,3 @@ function fetch(event) {
 
 let elementForm = document.querySelector("#generator");
 elementForm.addEventListener("submit", fetch);
-
-//api wikipedia for pictures
